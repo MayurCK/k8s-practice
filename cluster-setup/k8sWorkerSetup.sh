991f7de8fc769b5372d3fec32c75037ff84fa6dc
root <root@instance-1.c.kubernetes-p1.internal>
2018-08-30 07:58:19 +0000

    added k8s repo

--- cluster-setup/k8sWorkerSetup.sh
+++ cluster-setup/k8sWorkerSetup.sh
@@ -13,6 +13,15 @@ sudo firewall-cmd --add-port=6783/tcp
 printMsg "Opening ports in firewall done successfully !!"
 sudo modprobe br_netfilter
 sudo echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
 printMsg "Kubernetes repo is created successfully !!"
 sudo yum -y install conntrack
 printMsg "conntrack is installed successfully !!"
